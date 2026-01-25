1be9c6e516b94769759fb5863d4e2dfbc70c8707
Ismael Sosa Domínguez <ismael@nextchapter-ecommerce.com>
2026-01-25 09:26:50 +0100

    Set custom application icon to chalkboard-teacher.ico
    Added chalkboard-teacher.ico as the app icon in project settings and resources. Updated Form1 to use the new icon, and embedded it in Form1.resx. This enhances the application's branding and visual identity.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,86 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lstStudents = new System.Windows.Forms.ListBox();
-            this.btnSelectFile = new System.Windows.Forms.Button();
-            this.lblSelectedFile = new System.Windows.Forms.Label();
-            this.btnGenerate = new System.Windows.Forms.Button();
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
-            this.statusStrip1.SuspendLayout();
-            this.SuspendLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            lstStudents = new ListBox();
+            btnSelectFile = new Button();
+            lblSelectedFile = new Label();
+            btnGenerate = new Button();
+            statusStrip1 = new StatusStrip();
+            lblStatus = new ToolStripStatusLabel();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
             // 
             // lstStudents
             // 
-            this.lstStudents.FormattingEnabled = true;
-            this.lstStudents.ItemHeight = 15;
-            this.lstStudents.Location = new System.Drawing.Point(12, 50);
-            this.lstStudents.Name = "lstStudents";
-            this.lstStudents.Size = new System.Drawing.Size(460, 244);
-            this.lstStudents.TabIndex = 0;
+            lstStudents.FormattingEnabled = true;
+            lstStudents.Location = new Point(12, 50);
+            lstStudents.Name = "lstStudents";
+            lstStudents.Size = new Size(460, 244);
+            lstStudents.TabIndex = 0;
             // 
             // btnSelectFile
             // 
-            this.btnSelectFile.Location = new System.Drawing.Point(12, 12);
-            this.btnSelectFile.Name = "btnSelectFile";
-            this.btnSelectFile.Size = new System.Drawing.Size(120, 23);
-            this.btnSelectFile.TabIndex = 1;
-            this.btnSelectFile.Text = "Select Master PDF";
-            this.btnSelectFile.UseVisualStyleBackColor = true;
-            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            btnSelectFile.Location = new Point(12, 12);
+            btnSelectFile.Name = "btnSelectFile";
+            btnSelectFile.Size = new Size(120, 23);
+            btnSelectFile.TabIndex = 1;
+            btnSelectFile.Text = "Select Master PDF";
+            btnSelectFile.UseVisualStyleBackColor = true;
+            btnSelectFile.Click += btnSelectFile_Click;
             // 
             // lblSelectedFile
             // 
-            this.lblSelectedFile.AutoSize = true;
-            this.lblSelectedFile.Location = new System.Drawing.Point(140, 16);
-            this.lblSelectedFile.Name = "lblSelectedFile";
-            this.lblSelectedFile.Size = new System.Drawing.Size(91, 15);
-            this.lblSelectedFile.TabIndex = 2;
-            this.lblSelectedFile.Text = "No file selected";
+            lblSelectedFile.AutoSize = true;
+            lblSelectedFile.Location = new Point(140, 16);
+            lblSelectedFile.Name = "lblSelectedFile";
+            lblSelectedFile.Size = new Size(88, 15);
+            lblSelectedFile.TabIndex = 2;
+            lblSelectedFile.Text = "No file selected";
             // 
             // btnGenerate
             // 
-            this.btnGenerate.Location = new System.Drawing.Point(12, 305);
-            this.btnGenerate.Name = "btnGenerate";
-            this.btnGenerate.Size = new System.Drawing.Size(460, 35);
-            this.btnGenerate.TabIndex = 3;
-            this.btnGenerate.Text = "Generate Feedback Emails";
-            this.btnGenerate.UseVisualStyleBackColor = true;
-            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            btnGenerate.Location = new Point(12, 305);
+            btnGenerate.Name = "btnGenerate";
+            btnGenerate.Size = new Size(460, 35);
+            btnGenerate.TabIndex = 3;
+            btnGenerate.Text = "Generate Feedback Emails";
+            btnGenerate.UseVisualStyleBackColor = true;
+            btnGenerate.Click += btnGenerate_Click;
             // 
             // statusStrip1
             // 
-            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.lblStatus});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 350);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(484, 22);
-            this.statusStrip1.TabIndex = 4;
-            this.statusStrip1.Text = "statusStrip1";
+            statusStrip1.Items.AddRange(new ToolStripItem[] { lblStatus });
+            statusStrip1.Location = new Point(0, 350);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(484, 22);
+            statusStrip1.TabIndex = 4;
+            statusStrip1.Text = "statusStrip1";
             // 
             // lblStatus
             // 
-            this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(39, 17);
-            this.lblStatus.Text = "Ready";
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(39, 17);
+            lblStatus.Text = "Ready";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 372);
-            this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.btnGenerate);
-            this.Controls.Add(this.lblSelectedFile);
-            this.Controls.Add(this.btnSelectFile);
-            this.Controls.Add(this.lstStudents);
-            this.Name = "Form1";
-            this.Text = "Feedback Flow - Teacher Assistant";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.statusStrip1.ResumeLayout(false);
-            this.statusStrip1.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(484, 372);
+            Controls.Add(statusStrip1);
+            Controls.Add(btnGenerate);
+            Controls.Add(lblSelectedFile);
+            Controls.Add(btnSelectFile);
+            Controls.Add(lstStudents);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Feedback Flow - Teacher Assistant";
+            Load += Form1_Load;
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
 
         }
 
